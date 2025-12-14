f7b0ad3f92d9cc231c4c2fd5cbda93bfbe803514
webybuild <148471132+webybuild@users.noreply.github.com>
2025-12-15 01:05:00 +0530

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 // const url = 'http://localhost:3000';
 // const url2 = 'http://localhost:4000';
-const url = "https://h5w6vogo48.execute-api.us-east-1.amazonaws.com/prod"; //for normal data
+const url = "https://h2ldowngw9.execute-api.us-east-1.amazonaws.com/main"; //for normal data
 // const url = 'http://44.204.81.131:3000';
-const url2 = "https://h5w6vogo48.execute-api.us-east-1.amazonaws.com/prod"; //for images
+const url2 = "https://h2ldowngw9.execute-api.us-east-1.amazonaws.com/main"; //for images
 
 export { url, url2 };
