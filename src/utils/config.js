c7602f0ee2f483fb0c0f65f0361b1ae878301d57
knlkamal32 <knlkamal32@gmail.com>
2026-02-16 01:42:30 +0530

    product mod

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
-const url = 'http://localhost:3000';
-const url2 = 'http://localhost:3000';
-// const url = "https://h2ldowngw9.execute-api.us-east-1.amazonaws.com/main"; //for normal data
+// const url = 'http://localhost:3000';
+// const url2 = 'http://localhost:3000';
+const url = "https://h2ldowngw9.execute-api.us-east-1.amazonaws.com/main"; //for normal data
 // const url = 'http://44.204.81.131:3000';
-// const url2 = "https://h2ldowngw9.execute-api.us-east-1.amazonaws.com/main"; //for images
+const url2 = "https://h2ldowngw9.execute-api.us-east-1.amazonaws.com/main"; //for images
 
 export { url, url2 };
